64db04980a1e695f3025f0c215d649b90100bd57
Michael Callahan <michael@callahan.us>
2020-05-18 11:39:49 -0400

    made changes

--- 1.34-alpine-azure/docker-entrypoint.sh
+++ 1.34-alpine-azure/docker-entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-php-fpm7 -F
-nginx -g "daemon off"
+ln -s /data/images /var/www/html/images
+ln -s /data/LocalSettings.php /var/www/html/LocalSettings.php
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
